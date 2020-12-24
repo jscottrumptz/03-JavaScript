eaf58c5707b80b5c27419f65e3f134de76baca5e
J Scott Rumptz <jscott@rumptz.tech>
2020-12-24 09:24:30 -0600

    playing with user input tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// String variables holding every possible character value for each character type
 let lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let specialChar = "!@#$%^&*()_}{[]"
+let specialChars = "!@#$%^&*()_}{[]"
+let numeric = "0123456789"
 
-function getRandomInt(max) {
-return Math.floor(Math.random() * Math.floor(max));
+// Function that generates a random character from an array or string value
+function getRandomCharacter(characterType) {
+  return characterType[Math.floor(Math.random() * Math.floor(characterType.length))];
 }
 
-function getRandomItem(type) {
-let max = type.length;
-return type[Math.floor(Math.random() * Math.floor(max))];
+// Function that generates a random password based on user input and selection criteria
+function generatePassword() {
+  // Get desired password length from the user
+  let passwordLength = prompt("How many characters would you like your password?");
+  // Check to see if the password is too short or too long
+  if ((passwordLength < 8)||(passwordLength > 128)){
+    alert("Please enter a numeric value between 8-128")
+    generatePassword()
+  } 
+  // Get desired character types to include in the password
+  let num = confirm("Would you like to include Numbers?");
+  let lCase = confirm("Would you like to include Lower Case Letters?");
+  let uCase = confirm("Would you like to include Upper Case Letters?");
+  let spChar = confirm("Would you like to include Special Characters?");
+  // Check to see if at least one character type was selected
+  if ((num === false) && (lCase === false) && (uCase === false) && (spChar === false)){
+    alert("Please pick at least on character type");
+    generatePassword();
+  } else {
+
+    return "here is your password";
+  }
+}
+
+// BEGIN Testing getRandomCharacter function to console
+for ( let i = 0; i < 10; i++) {
+  console.log(getRandomCharacter(numeric));
 }
 
 for ( let i = 0; i < 10; i++) {
-  console.log(getRandomInt(10));
+  console.log(getRandomCharacter(lowerLetters));
 }
 
 for ( let i = 0; i < 10; i++) {
-  console.log(getRandomItem(lowerLetters));
+  console.log(getRandomCharacter(upperLetters));
 }
 
 for ( let i = 0; i < 10; i++) {
-  console.log(getRandomItem(upperLetters));
+  console.log(getRandomCharacter(specialChars));
 }
-
-for ( let i = 0; i < 10; i++) {
-  console.log(getRandomItem(specialChar));
-}
+// END Testing getRandomCharacter function to console
 
 // Write password to the #password input
 function writePassword() {
