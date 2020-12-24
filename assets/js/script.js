9a6174eb3c9d8721f78a74abb264a7cb5db57cb2
J Scott Rumptz <jscott@rumptz.tech>
2020-12-24 10:09:19 -0600

    input validation complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Set default values for password length and included character types
+let passwordLength = 0;
+let num = false;
+let lCase = false;
+let uCase = false;
+let spChar = false;
+
 // String variables holding every possible character value for each character type
 let lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let specialChars = "!@#$%^&*()_}{[]"
+let specialChars =  "!#$%&'()*+,-./:;<=>?@[^_`{|}~"
 let numeric = "0123456789"
 
 // Function that generates a random character from an array or string value
@@ -12,28 +19,32 @@ function getRandomCharacter(characterType) {
   return characterType[Math.floor(Math.random() * Math.floor(characterType.length))];
 }
 
-// Function that generates a random password based on user input and selection criteria
-function generatePassword() {
+function lengthInputTest(){
   // Get desired password length from the user
-  let passwordLength = prompt("How many characters would you like your password?");
+  passwordLength = prompt("How many characters would you like your password?");
   // Check to see if the password is too short or too long
   if ((passwordLength < 8)||(passwordLength > 128)){
     alert("Please enter a numeric value between 8-128")
-    generatePassword()
+    lengthInputTest()
   } 
+}
+
+function typeInputTest(){
   // Get desired character types to include in the password
-  let num = confirm("Would you like to include Numbers?");
-  let lCase = confirm("Would you like to include Lower Case Letters?");
-  let uCase = confirm("Would you like to include Upper Case Letters?");
-  let spChar = confirm("Would you like to include Special Characters?");
+  num = confirm("Would you like to include Numbers?");
+  lCase = confirm("Would you like to include Lower Case Letters?");
+  uCase = confirm("Would you like to include Upper Case Letters?");
+  spChar = confirm("Would you like to include Special Characters?");
   // Check to see if at least one character type was selected
   if ((num === false) && (lCase === false) && (uCase === false) && (spChar === false)){
     alert("Please pick at least on character type");
-    generatePassword();
-  } else {
-
-    return "here is your password";
+    typeInputTest()
   }
+}
+
+// Function that generates a random password based on user input and selection criteria
+function generatePassword() {
+  return "Generated Password"
 }
 
 // BEGIN Testing getRandomCharacter function to console
