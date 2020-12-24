9a9438d7ff50266e88ed8d5c805edb6f69bfb2a4
J Scott Rumptz <jscott@rumptz.tech>
2020-12-23 21:28:48 -0600

    playing with random ideas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+let lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let specialChar = "!@#$%^&*()_}{[]"
+
+function getRandomInt(max) {
+return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomItem(type) {
+let max = type.length;
+return type[Math.floor(Math.random() * Math.floor(max))];
+}
+
+for ( let i = 0; i < 10; i++) {
+  console.log(getRandomInt(10));
+}
+
+for ( let i = 0; i < 10; i++) {
+  console.log(getRandomItem(lowerLetters));
+}
+
+for ( let i = 0; i < 10; i++) {
+  console.log(getRandomItem(upperLetters));
+}
+
+for ( let i = 0; i < 10; i++) {
+  console.log(getRandomItem(specialChar));
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +41,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
